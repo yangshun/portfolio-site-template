59ee30817e03a1ba7e3e17596020aa4a692e4d9b
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-09-08 02:30:10 +0800

    Main and portfolio page done

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('lab1App', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'LandingCtrl'
+      })
+      .when('/portfolio', {
+        templateUrl: 'views/portfolio.html',
+        controller: 'PortfolioCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+        controller: 'LandingCtrl'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var NAME = 'Yang Shun';
+
+angular.module('lab1App')
+  .filter('to_trusted', ['$sce', function ($sce){
+    return function (text) {
+      return $sce.trustAsHtml(text);
+    };
+  }])
+  .controller('AppCtrl', function ($scope) {
+    $scope.name = NAME;
+  })
+  .controller('LandingCtrl', function ($scope) {
+
+  })
+  .controller('PortfolioCtrl', function ($scope, $http) {
+    $http({method: 'GET', url: '/portfolio/portfolio.json'}).
+      success(function(data, status, headers, config) {
+        $scope.portfolio = data.data;
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+  });
+
